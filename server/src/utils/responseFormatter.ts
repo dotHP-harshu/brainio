4a9d1d647b138b622343b839ed43d6749a5e47ea
Harsh Prajapati <harshprajapaticoding@gmail.com>
2026-02-01 16:47:59 +0530

    server: added response formatter

---
+++ server/src/utils/responseFormatter.ts
@@ -0,0 +1,10 @@
+import { Response } from "express"
+
+const sendResponse =(res:Response, data:any,  message:string, success:boolean = true, status:number=200)=>{
+    res.send({data, message, success, status})
+}
+const sendError =(res:Response, err:Error,  message:string, success:boolean = false, status:number)=>{
+    res.send({err, message, success, status})
+}
+
+export {sendError, sendResponse}
