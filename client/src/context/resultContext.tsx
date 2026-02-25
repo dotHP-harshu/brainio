6b318d88bbbfd1fa57edc8ea1b971c06bc2827d0
Harsh Prajapati <harshprajapaticoding@gmail.com>
2026-02-25 21:47:46 +0530

    client: added the functionality of saving the test and result in session storage

--- client/src/context/resultContext.tsx
+++ client/src/context/resultContext.tsx
@@ -1,25 +1,42 @@
-import { createContext, useContext, useState, type PropsWithChildren } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  type PropsWithChildren,
+} from "react";
 import type { ResultContextInterface } from "../types/types";
 
-const resultContext = createContext<ResultContextInterface | null>(null)
+const resultContext = createContext<ResultContextInterface | null>(null);
 
 export const ResultContextProvider = ({ children }: PropsWithChildren) => {
+  const [testResult, setTestResult] = useState<
+    ResultContextInterface["testResult"]
+  >(() => {
+    const result = window.sessionStorage.getItem("brainio_result");
+    if (result === "null") return null;
+    if (result)
+      return JSON.parse(result) as ResultContextInterface["testResult"];
+    return null;
+  });
 
-    const [testResult, setTestResult] = useState<ResultContextInterface["testResult"]>(null)
+  const getCurrentResult = (result: ResultContextInterface["testResult"]) => {
+    setTestResult(result);
+    window.sessionStorage.setItem("brainio_result", JSON.stringify(result));
+  };
 
-
-    return (
-        <resultContext.Provider value={{ testResult, setTestResult }} >
-            {children}
-        </resultContext.Provider>
-    )
-
-}
+  return (
+    <resultContext.Provider
+      value={{ testResult, setTestResult: getCurrentResult }}
+    >
+      {children}
+    </resultContext.Provider>
+  );
+};
 
 export const useResultContext = () => {
-    const ctx = useContext(resultContext)
-    if (!ctx) {
-        throw new Error("Result context is not available")
-    }
-    return ctx
-}
+  const ctx = useContext(resultContext);
+  if (!ctx) {
+    throw new Error("Result context is not available");
+  }
+  return ctx;
+};
--- client/src/context/testContext.tsx
+++ client/src/context/testContext.tsx
@@ -1,25 +1,38 @@
-import { createContext, useContext, useState, type PropsWithChildren } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  type PropsWithChildren,
+} from "react";
 import type { TestContextInterface } from "../types/types";
 
 export const testContext = createContext<TestContextInterface | null>(null);
 
 export const TestContextProvider = ({ children }: PropsWithChildren) => {
-    const [test, setTest] = useState<TestContextInterface["test"]>(null);
-    
+  const [test, setTest] = useState<TestContextInterface["test"]>(() => {
+    const test = window.sessionStorage.getItem("brainio_test");
+    if (test === "null") return null;
+    if (test) return JSON.parse(test) as TestContextInterface["test"];
+    return null;
+  });
 
-    return (
-        <testContext.Provider value={{ test, setTest }}>
-            {children}
-        </testContext.Provider>
-    );
+  const setCurrentTest = (test: TestContextInterface["test"]) => {
+    setTest(test);
+    window.sessionStorage.setItem("brainio_result", JSON.stringify(null));
+    window.sessionStorage.setItem("brainio_test", JSON.stringify(test));
+  };
 
-}
-
+  return (
+    <testContext.Provider value={{ test, setTest: setCurrentTest }}>
+      {children}
+    </testContext.Provider>
+  );
+};
 
 export const useTestContext = () => {
-    const context = useContext(testContext);
-    if (!context) {
-        throw new Error("useTestContext must be used within a TestContextProvider");
-    }
-    return context;
+  const context = useContext(testContext);
+  if (!context) {
+    throw new Error("useTestContext must be used within a TestContextProvider");
+  }
+  return context;
 };
