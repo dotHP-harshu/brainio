c744cc9b14a867fad9a002b0b156105ed6d67f18
Harsh Prajapati <harshprajapaticoding@gmail.com>
2026-03-03 20:53:26 +0530

    client: added some animation on the home page

--- client/src/components/Home/FeatureSection.tsx
+++ client/src/components/Home/FeatureSection.tsx
@@ -1,17 +1,22 @@
 import { Bot, ChartBarIncreasing, CloudBackup, FileQuestionMark, Lightbulb } from "lucide-react";
+import {motion} from "framer-motion"
 
 function FeatureSection() {
   return (
     <section className="py-20 bg-white border-y-4 border-text relative">
       <div className="absolute inset-0 grid-bg-pattern"></div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
-        <div className="text-center mb-16">
-          <h2 className="text-4xl md:text-5xl font-black uppercase mb-4 bg-white inline-block px-4 border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+        <div className="text-center mb-16 ">
+          <motion.h2 initial={{scale:0, y:100}} whileInView={{scale:1, y:0}} className="text-4xl md:text-5xl font-black uppercase mb-4 bg-white inline-block px-4 border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
             Built for Retention
-          </h2>
+          </motion.h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-6 gap-6 md:gap-8 auto-rows-[minmax(200px,auto)]">
-          <div className="col-span-1 md:col-span-4 neo-card bg-teal-500 p-8  flex flex-col md:flex-row items-center gap-6 overflow-hidden relative group">
+          <motion.div  
+          initial={{opacity:0, y:50}}
+          whileInView={{opacity:1, y:0}}
+          transition={{duration:1}}
+          className="col-span-1 md:col-span-4 neo-card bg-teal-500 p-8  flex flex-col md:flex-row items-center gap-6 overflow-hidden relative group">
             <div className="flex-1 relative z-10 text-white">
               <div className="w-16 h-16 bg-white border-4 border-black rounded-full flex items-center justify-center mb-4 text-text">
                 <span>
@@ -34,8 +39,12 @@ function FeatureSection() {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-span-1 md:col-span-2 neo-card bg-secondary p-8  flex flex-col justify-between">
+          </motion.div>
+          <motion.div  
+          initial={{opacity:0, y:50}}
+          whileInView={{opacity:1, y:0}}
+          transition={{duration:1}}
+          className="col-span-1 md:col-span-2 neo-card bg-secondary p-8  flex flex-col justify-between">
             <div>
               <span>
                 <CloudBackup />
@@ -47,8 +56,12 @@ function FeatureSection() {
                 Save locally or sync to your Google account.
               </p>
             </div>
-          </div>
-          <div className="col-span-1 md:col-span-2 neo-card bg-gray-300 p-8  flex flex-col justify-between">
+          </motion.div>
+          <motion.div  
+          initial={{opacity:0, y:50}}
+          whileInView={{opacity:1, y:0}}
+          transition={{duration:1}}
+          className="col-span-1 md:col-span-2 neo-card bg-gray-300 p-8  flex flex-col justify-between">
             <div>
               <span className="text-primary"> 
                 <FileQuestionMark/>
@@ -60,8 +73,12 @@ function FeatureSection() {
                 MCQs for facts, Q&amp;A for depth.
               </p>
             </div>
-          </div>
-          <div className="col-span-1 md:col-span-4 neo-card bg-primary p-8  text-white relative overflow-hidden">
+          </motion.div>
+          <motion.div  
+          initial={{opacity:0, y:50}}
+          whileInView={{opacity:1, y:0}}
+          transition={{duration:1}}
+          className="col-span-1 md:col-span-4 neo-card bg-primary p-8  text-white relative overflow-hidden">
             <div className="relative z-10">
               <div className="flex items-center gap-4 mb-4">
                 <span className="text-white bg-black">
@@ -85,7 +102,7 @@ function FeatureSection() {
             <span className="material-symbols-outlined absolute -bottom-10 -right-10 text-[10rem] opacity-20 text-black">
               insights
             </span>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
