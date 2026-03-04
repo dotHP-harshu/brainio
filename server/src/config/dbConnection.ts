1541d57a419a794db55cf63b56ad0bfcf430165e
Harsh Prajapati <harshprajapaticoding@gmail.com>
2026-03-04 09:30:31 +0530

    server: make the db connection async

--- server/src/config/dbConnection.ts
+++ server/src/config/dbConnection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import config from "./config";
 
-export default function dbConnection (){
-  return mongoose.connect(config.MONGOOSE_URI,)
+export default async function dbConnection (){
+  return await mongoose.connect(config.MONGOOSE_URI,)
 }
